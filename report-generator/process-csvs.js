7daed3e0402722ba48f8d8fd557a2382e4679562
Alexandre Swetlowski <>
2023-05-18 12:01:55 -0400

    mapping generator is working well. still a few things to clean up

--- report-generator/process-csvs.js
+++ report-generator/process-csvs.js
@@ -1,9 +1,11 @@
+const util = require('util')
 const fs = require("fs");
 const { parse } = require("csv-parse");
 let lighthouseCSVimport = [];
 let reportObject = {};
 let reportHeaders = [];
 let fieldString = [];
+let reportMappings = [];
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 function cleanTitles(title) {
     let theTitle;
@@ -11,6 +13,25 @@ function cleanTitles(title) {
     theTitle = theTitle.replace(/[^a-z," "]/gi, '');
     return theTitle;
     // theTitle = theTitle.replaceAll('', '-');
+}
+function createMappings(key) {
+    // console.log('HERE!!!!!!!!!', key);
+    let mapping = [];
+    let prefix = '';
+    key = key.replace('field_', '_');
+    if(key!='title') { 
+        prefix = 'field_';
+    }
+
+    mapping.push('  -');
+    mapping.push('    target: ' + prefix + key.substring(0,26));
+    mapping.push('    map:');
+    mapping.push('      value: ' + key);
+    mapping.push('    settings:');
+    mapping.push('      language: null');
+    mapping.push('    unique: {  }');
+
+    return mapping;
 }
 function fieldType(type) {
     let theType = new String;
@@ -41,8 +62,28 @@ fs.createReadStream("test.csv")
         let key = r[3];
         key = key.replaceAll("-", "_");
         reportObject[key] = r[6];
+        reportObject.requested_url = r[0];
+        reportObject.final_url = r[1];
+
+        let domain = (new URL(reportObject.final_url));
+        reportObject.title = domain.hostname.replace('www.', '');
+        // console.log('reportObject.title: ', reportObject.title);
         // console.log(reportObject);
         reportHeaders.push({id: key, title: key});
+
+
+        reportMappings = reportMappings.concat(createMappings(key));
+
+
+        // reportMappings.push('  -');
+        // reportMappings.push('    target: field_' + key.substring(0,26));
+        // reportMappings.push('    map:');
+        // reportMappings.push('      value: ' + key);
+        // reportMappings.push('    settings:');
+        // reportMappings.push('      language: null');
+        // reportMappings.push('    unique: {  }');
+
+        
         fieldString.push(key);
         fieldString.push('|');
         fieldString.push(cleanTitles(r[4]));
@@ -50,7 +91,20 @@ fs.createReadStream("test.csv")
         fieldString.push(fieldType(r[5]));
         fieldString.push('\n');
     });
-    console.log(fieldString.join(''));
+    
+
+    reportMappings = reportMappings.concat(createMappings('title'));
+    
+    // console.log(util.inspect(reportMappings, { maxArrayLength: null }))
+
+    console.log(reportMappings.join('\n'));
+
+    reportHeaders.push({id: 'title', title: 'title'});
+    reportHeaders.push({id: 'requested_url', title: 'requested_url'});
+    reportHeaders.push({id: 'final_url', title: 'final_url'});
+
+
+
     writeToCSV('_test-write-csv.csv', reportHeaders, [reportObject]);
   });
   
