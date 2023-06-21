45328b573bfec6f9e789fb418d2265eac0dad7c2
Alexandre Swetlowski <>
2023-06-21 11:42:02 -0400

    sortable stuff.

--- themes/custom/auto_mate_001/js/global.js
+++ themes/custom/auto_mate_001/js/global.js
@@ -9,5 +9,15 @@ console.log(1);
     $(document).ready(function(){
         'use strict';
         console.log(3);
+        // views-field-title
+        // $('.view-content').sortable();
+        window.setTimeout(function(){
+            $('.view-lighthouse-reports .view-content tbody').attr('id', 'lightHouseReports');
+            new Sortable(lightHouseReports, {
+                animation: 150,
+                ghostClass: 'blue-background-class'
+            });
+    
+        }, 2000);
     });
 })(jQuery);
