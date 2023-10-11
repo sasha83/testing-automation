31c462dcdad4b5a56414c407c1c48a1e89adf574
Alexandre Swetlowski <>
2023-10-11 11:47:09 -0400

    fix package.json styling checks lots of JSResources sidebar work

--- themes/custom/auto_mate_react_001/js/src/Components/JSResources.js
+++ themes/custom/auto_mate_react_001/js/src/Components/JSResources.js
@@ -19,36 +19,68 @@ import PercentageMeter from '../UIElements/PercentageMeter';
     // meter for each selected script
     // used, total
 
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function JSResources(props) {
     // if(props["activeJSResourcesURLs"]&&props["activeJSResourcesURLs"].length>0) {
     const uiState=props["uiState"];
     const GlobalState=props["GlobalState"];
     const urlData=GlobalState.urlData;
     const LHRData=GlobalState.LHRData;
-    console.log('urlData:', urlData);
-    console.log('LHRData:', LHRData);
+    // console.log('urlData:', urlData);
+    // console.log('LHRData:', LHRData);
     if(uiState.activeJSResourcesURLs.length>0) {
         const activeJSResourcesURLs=uiState.activeJSResourcesURLs;
-        console.log('activeJSResourcesURLs:', activeJSResourcesURLs);
+        // console.log('activeJSResourcesURLs:', activeJSResourcesURLs);
         const urlList = activeJSResourcesURLs.map((urlID, index) => {
             const url=urlData.filter((urlDat) => urlDat.nid==urlID);
-            console.log('url:', url);
+            // console.log('url:', url);
             return <tr key={url[0].nid}><td><Checkbox/></td><td>{url[0].nid}</td><td>{url[0].title}</td></tr>;
         });
 
-        
-        const sharedJSResources=activeJSResourcesURLs.map((urlID, index) => {
+        // JSON.parse(data[0][field_name].replaceAll('&quot;', '"'));
+        const resourceNodesByID=activeJSResourcesURLs.map((urlID, index) => {
             const mostRecent=LHRData.filter((LHRDat) => parseInt(LHRDat.field_url_reference_1)==parseInt(urlID));
-            console.log('mostRecent:', mostRecent, urlID);            
-            return mostRecent[0];
+            // console.log('mostRecent[0].field_script_treemap_data:', JSON.parse(mostRecent[0].field_script_treemap_data.replaceAll('&quot;', '"')), urlID);            
+            return {nid: urlID, nodes: JSON.parse(mostRecent[0].field_script_treemap_data.replaceAll('&quot;', '"')).nodes};
         });
-
+        console.log('resourceNodesByID:', resourceNodesByID);
+        let globalJSResources=[];
+        let uniqueJSResources=[];
+        resourceNodesByID.map((url, index) => {
+            console.log('url:', url);
+            // uniqueJSResources.push({nid: url.nid, nodes: []});
+            url.nodes.map((JSnode, JSnodeIndex) => {
+                const findName = globalJSResources.filter((resource) => resource.name==JSnode.name);
+                if(findName.length==0) globalJSResources.push({name: JSnode.name, bytes: JSnode.resourceBytes});
+                // if(globalJSResources.indexOf(JSnode.name)==-1) globalJSResources.push({name: JSnode.name, bytes: JSnode.resourceBytes});
+            });
+        });
+        function compareBytes( a, b ) {
+            if ( a.bytes < b.bytes ){
+                return 1;
+            }
+            if ( a.bytes > b.bytes ){
+                return -1;
+            }
+            return 0;
+        }
+          
+        globalJSResources.sort( compareBytes );
+          
+        const globalJSNodes=globalJSResources.map((node, index) => {
+            return (<tr><Checkbox/><td className='js-resource-name'>{node.name}</td><td className='js-resource-size'>{numberWithCommas(node.bytes)}</td></tr>);
+        });
+        
+        console.log('globalJSResources:', globalJSResources);
         return (<div className='block block-js-resources-block'>
             <BlockTitle title="JavaScript Resources"/>
             <BlockContent
                 blockName="js-resources"
                 content={
-                    <table><tbody>{urlList}</tbody></table>
+                    <table><tbody>{globalJSNodes}</tbody></table>
                 }
             /></div>);
     
