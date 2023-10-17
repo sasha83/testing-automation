3dc5deb024dcf633ae92756ec589b3715591aa89
Alexandre Swetlowski <>
2023-10-17 09:58:50 -0400

    timeline labels

--- themes/custom/auto_mate_react_001/js/src/Components/DomainTimeline.js
+++ themes/custom/auto_mate_react_001/js/src/Components/DomainTimeline.js
@@ -4,6 +4,7 @@ import BlockContent from '../UIElements/BlockContent';
 import Checkbox from '@mui/material/Checkbox';
 import PercentageMeter from '../UIElements/PercentageMeter';
 import './DomainTimeline.css';
+import classNames from 'classnames';
 // import './JSResources.css';
 
 const oneWeek = 60*60*1000*24*7;
@@ -13,6 +14,7 @@ const oneMinute = 60*1000;
 const defaultTLConfig = {
         durationEffectsHeight: true,
         elementAlignment: 'top',
+        showLabels: false,
         grid: {
                 weekMarkers: true,
                 dayMarkers: true,
@@ -49,6 +51,7 @@ const defaultTLConfig = {
 const defaultTLConfigB = {
         durationEffectsHeight: true,
         elementAlignment: 'bottom',
+        showLabels: true,
         grid: {
                 weekMarkers: true,
                 dayMarkers: true,
@@ -99,12 +102,14 @@ function Timeline(props) {
         console.log("lastMidnight:", lastMidnight, lastMidnight.getTime());
 
 
-        return (<>
-                <TimelineGrid timeScale={timeScale} grid={grid}/>
+        return (<div className={classNames(['timeline', 'align-'+elementAlignment])}>
+                <TimelineGrid timeScale={timeScale} grid={grid} tlconfig={tlconfig}/>
                 <TimelineElements elementAlignment={elementAlignment} timelineElements={timelineElements}/>
-        </>);
+        </div>);
 }
 function TimelineElements(props) {
+        const timelineElements = props["timelineElements"];
+        console.log('timelineElements:', timelineElements);
         return (<><TimelineElement x={-1*oneDay}/></>);
 }
 function TimelineElement(props) {
@@ -114,7 +119,7 @@ function TimelineElement(props) {
 function TimelineGrid(props) {
         const grid = props['grid'];
         const timeScale=props["timeScale"];
-        
+        const tlconfig=props["tlconfig"];
         console.log('TimelineGrid grid', grid);
         console.log('timeScale:', timeScale);
         const gridLineStyle = {
@@ -125,13 +130,14 @@ function TimelineGrid(props) {
 
         return (<div className='timeline-grid-container'>
                 <div className='timeline-grid'>
-                        <GridLines timeScale={timeScale} gridLineStyle={gridLineStyle} grid={grid}/>
+                        <GridLines timeScale={timeScale} gridLineStyle={gridLineStyle} grid={grid} tlconfig={tlconfig}/>
                 </div>
         </div>);
 }
 function GridLines(props) {
         const timeScale = props["timeScale"];
         const gridLineStyle = props["gridLineStyle"];
+        const showLabels = props["tlconfig"].showLabels;
         const grid = props['grid'];
         const nowMS = new Date().getTime();
         const now = new Date(nowMS);
@@ -148,20 +154,26 @@ function GridLines(props) {
                 const theDay = new Date(lastMidnight-n);
                 lines.push({
                         day: theDay,
-                        agoPerc: (n+timeSinceMidnight)/timeScale*100
+                        dayOfWeek: theDay.getDay(),
+                        agoPerc: (n+timeSinceMidnight)/timeScale*100,
                 })
                 console.log("GridLines n:", n);
                 console.log("GridLines nPerc:", nPerc);
         }
-        console.log(lines);
+        console.log('lines:', lines);
         const gridLines = lines.map((line) => {
-                return (<GridLine x={line.agoPerc+"%"} gridLineStyle={gridLineStyle}/>);
+                return (<GridLine x={line.agoPerc+"%"} gridLineStyle={gridLineStyle} label={line.dayOfWeek} date={line.day} showLabels={showLabels}/>);
         });
         return (<>{gridLines}</>);
 }
 function GridLine(props) {
+        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        const showLabels = props["showLabels"];
         // console.log('gridLineStyle:', gridLineStyle);
         // gridLineStyle.right = props["x"];
+        const label = props['label'];
+        const theDate = props['date'];
+        const date = theDate.getDate();
         const gridLineStyle = {
                 position: 'absolute',
                 backgroundColor: "#333",
@@ -169,7 +181,28 @@ function GridLine(props) {
                 width: "1px",
                 right: props["x"]
         };                
-        return (<div className='grid-line' style={gridLineStyle}></div>);
+
+
+
+
+
+
+        // if timeScale is greater than 2 weeks
+                // then don't show every day label
+        // if timeScale is greater than 1 month
+                // then don't show day lines
+        // if timeScale is greater than 3 months
+                // then show Monday lines with numbers
+
+
+
+
+        return (<div className='grid-line' style={gridLineStyle}>
+
+                {/* {(showLabels==true) && <span class="grid-line-label">{weekday[label]}</span>} */}
+                {(showLabels==true) && <span class="grid-line-label">{JSON.stringify(date)}</span>}
+                
+        </div>);
 }
 
 
@@ -177,14 +210,14 @@ function GridLine(props) {
 function Events() {
         const tlconfig = defaultTLConfigB;
         return (<>
-                <Timeline timeScale={oneWeek*1.5} timeLineConfiguration={tlconfig} />
+                <Timeline timeScale={oneWeek*3} timeLineConfiguration={tlconfig} />
         </>);
 }
  
 function Instances() {
         const tlconfig = defaultTLConfig;
         return (<>
-                <Timeline timeScale={oneWeek*1.5} timeLineConfiguration={tlconfig} />
+                <Timeline timeScale={oneWeek*3} timeLineConfiguration={tlconfig} />
         </>);
 }
  
